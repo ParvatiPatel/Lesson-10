8c8efb5488beb3e34b23d1427dc930864dcf56ac
ParvatiPatel <paro.patel@rocketmail.com>
2016-07-21 13:33:36 -0400

    Add project files.

---
+++ COMP2007-S2016-Lesson10paro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP2007_S2016_Lesson10paro.Startup))]
+namespace COMP2007_S2016_Lesson10paro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
